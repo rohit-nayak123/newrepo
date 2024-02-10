b78d94ae0ccfe3f30712b1df9534159f1f80612d
rohit-nayak123 <113372030+rohit-nayak123@users.noreply.github.com>
2024-02-10 07:12:29 +0530

    new changes

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+// vite.config.js
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about/index.html"),
+      },
+    },
+  },
+});
