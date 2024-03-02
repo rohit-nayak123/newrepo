d8580a16364df04101f78d21acb57bc6de96f2f3
rohit-nayak123 <113372030+rohit-nayak123@users.noreply.github.com>
2024-03-02 14:29:31 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
 // vite.config.js
 
-import { defineConfig } from "vite";
-import { resolve } from "path";
+import {defineConfig} from "vite";
+import {resolve} from "path";
 
 export default defineConfig({
-  base: "/newrepo/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "about.html"),
-        notes: resolve(__dirname, "notes.html"),
-        games: resolve(__dirname, "games/index.html"),
-        ticTackToe: resolve(__dirname, "games/tic-tack-toe.html"),
-      },
+    base: "/to-do/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                about: resolve(__dirname, "about.html"),
+                notes: resolve(__dirname, "notes.html"),
+                games: resolve(__dirname, "games/index.html"),
+                ticTackToe: resolve(__dirname, "games/tic-tack-toe.html"),
+            },
+        },
     },
-  },
 });
